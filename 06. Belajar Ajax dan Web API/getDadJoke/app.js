2facaaf181010ee006cb8866846e3fe10b2f58bc
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-09-04 11:52:00 +0700

    upload source code pembelajaran pada 06. Belajar Ajax dan Web Api

---
+++ 06. Belajar Ajax dan Web API/getDadJoke/app.js
@@ -0,0 +1,27 @@
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+
+console.log("aa");
+
+const addJoke = async () => {
+  const jokeText = await getJokes();
+  const newLI = document.createElement("LI");
+  newLI.append(jokeText);
+  jokes.append(newLI);
+};
+
+const getJokes = async () => {
+  try {
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await axios.get("https://icanhazdadjoke123123.com/", config);
+    return res.data.joke;
+  } catch (error) {
+    return "No Jokes Available!";
+  }
+};
+
+button.addEventListener("click", addJoke);
