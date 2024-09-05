1da2f5a279235401d0b5346ba0d0b6d02bf6f84e
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-09-05 22:49:20 +0700

    case: Memisahkan Component Pada Halaman Quiz

---
+++ 09. Membangun Website Reaktif dengan Vue.js 3/quiz-vue/src/components/QuizHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h4>Questions 1/2</h4>
+    <div class="bar">
+      <div class="bar-progress"></div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header h4 {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.bar {
+  width: 100%;
+  height: 10px;
+  border-radius: 5px;
+  background-color: #e0e0e0;
+  margin-top: 10px;
+}
+
+.bar-progress {
+  height: 100%;
+  width: 50%;
+  background-color: #adf7b6;
+  transition: width 0.5s ease;
+}
+</style>
