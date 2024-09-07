db0c84bacd364f3640dc926a4b2e005683b7024c
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-09-07 23:32:33 +0700

    case: Melakukan Parsing Data Dari Body Request

--- 12. Belajar RESTfull dengan Express.js/rest/index.js
+++ 12. Belajar RESTfull dengan Express.js/rest/index.js
@@ -2,11 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
+
 app.get("/order", (req, res) => {
   res.send("ini adalah request header response");
 });
 
 app.post("/order", (req, res) => {
+  console.log(req.body);
+  const { item, qty } = req.body;
+  res.send(`Item: ${item} dengan jumlah ${qty}`);
   res.send("ini adalah request header response");
 });
 
