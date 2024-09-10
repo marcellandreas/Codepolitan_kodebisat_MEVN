94306a0c6b76bea7e249b6c644f654e09327ef4f
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-09-10 22:39:00 +0700

    case: Latihan Restful Show Comment

--- 12. Belajar RESTfull dengan Express.js/rest/index.js
+++ 12. Belajar RESTfull dengan Express.js/rest/index.js
@@ -10,26 +10,32 @@ app.set("view engine", "ejs");
 
 let comments = [
   {
+    id: 1,
     username: "Michael",
     text: `Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way`,
   },
   {
+    id: 2,
     username: "Kelly",
     text: `I talk a lot, so I’ve learned to tune myself out`,
   },
   {
+    id: 3,
     username: "Kevin",
     text: `I JUST WANT TO LIE ON THE BEACH AND EAT HOT DOGS.`,
   },
   {
+    id: 4,
     username: "Dwight",
     text: `IDENTITY THEFT IS NOT A JOKE, JIM! MILLIONS OF FAMILIES SUFFER EVERY YEAR.`,
   },
   {
+    id: 5,
     username: "Ryan",
     text: `I’M SUCH A PERFECTIONIST THAT I'D KINDA RATHER NOT DO IT AT ALL THAN DO A CRAPPY VERSION.`,
   },
   {
+    id: 6,
     username: "Jim",
     text: `EVERYTHING I HAVE I OWE TO THIS JOB… THIS STUPID, WONDERFUL, BORING, AMAZING JOB.`,
   },
@@ -60,6 +66,12 @@ app.post("/order", (req, res) => {
   const { item, qty } = req.body;
   res.send(`Item: ${item} dengan jumlah ${qty}`);
   res.send("ini adalah request header response");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === parseInt(id));
+  res.render("comments/show", { comment });
 });
 
 app.listen(8080, () => {
