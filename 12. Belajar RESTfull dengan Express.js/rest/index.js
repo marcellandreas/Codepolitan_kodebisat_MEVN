706a086dc2a162e10289f1e3786f5f0261dbe4a6
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2024-09-10 22:45:35 +0700

    case: Mengamankan Nilai Id Dengan Uuid

--- 12. Belajar RESTfull dengan Express.js/rest/index.js
+++ 12. Belajar RESTfull dengan Express.js/rest/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-
+const { v4: uuidv4 } = require("uuid");
 const app = express();
 
 app.use(express.json());
@@ -10,32 +10,32 @@ app.set("view engine", "ejs");
 
 let comments = [
   {
-    id: 1,
+    id: uuidv4(),
     username: "Michael",
     text: `Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way`,
   },
   {
-    id: 2,
+    id: uuidv4(),
     username: "Kelly",
     text: `I talk a lot, so I’ve learned to tune myself out`,
   },
   {
-    id: 3,
+    id: uuidv4(),
     username: "Kevin",
     text: `I JUST WANT TO LIE ON THE BEACH AND EAT HOT DOGS.`,
   },
   {
-    id: 4,
+    id: uuidv4(),
     username: "Dwight",
     text: `IDENTITY THEFT IS NOT A JOKE, JIM! MILLIONS OF FAMILIES SUFFER EVERY YEAR.`,
   },
   {
-    id: 5,
+    id: uuidv4(),
     username: "Ryan",
     text: `I’M SUCH A PERFECTIONIST THAT I'D KINDA RATHER NOT DO IT AT ALL THAN DO A CRAPPY VERSION.`,
   },
   {
-    id: 6,
+    id: uuidv4(),
     username: "Jim",
     text: `EVERYTHING I HAVE I OWE TO THIS JOB… THIS STUPID, WONDERFUL, BORING, AMAZING JOB.`,
   },
@@ -52,9 +52,15 @@ app.get("/comments/create", (req, res) => {
 app.post("/comments", (req, res) => {
   const { username, text } = req.body;
   // comments.push({ ...comment });
-  comments.push({ username, text });
+  comments.push({ username, text, id: uuidv4() });
   // res.send("it's works");
   res.redirect("/comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
 });
 
 app.get("/order", (req, res) => {
@@ -66,12 +72,6 @@ app.post("/order", (req, res) => {
   const { item, qty } = req.body;
   res.send(`Item: ${item} dengan jumlah ${qty}`);
   res.send("ini adalah request header response");
-});
-
-app.get("/comments/:id", (req, res) => {
-  const { id } = req.params;
-  const comment = comments.find((c) => c.id === parseInt(id));
-  res.render("comments/show", { comment });
 });
 
 app.listen(8080, () => {
